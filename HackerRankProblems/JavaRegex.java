2d8ff7a0b4498ed3e0b63e903241300e7b32df16
kajalverma2404 <kajalverma2404@gmail.com>
2023-08-09 21:19:23 +0530

    JavaRegex

---
+++ HackerRankProblems/JavaRegex.java
@@ -0,0 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class JavaRegex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+
+class MyRegex {
+    public static final String IP_ADDRESS_PATTERN =
+        "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+        + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+        + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+        + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+    public static final String pattern = IP_ADDRESS_PATTERN;
+}
